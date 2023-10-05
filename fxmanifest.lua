c16af4647e52a3ff84ffc1e19e2374e479c2083d
Johnson6717 <johnsondeveloperfivem@gmail.com>
2023-10-05 02:20:37 -0300

    Se agrega al proyecto

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'LJ SCRIPTS'
+
+server_scripts {
+    'pack//server/*.lua'
+}
---
+++ server/server.lua
@@ -0,0 +1,21 @@
+local discordWebhook = "https://canary.discord.com/api/webhooks/1159038312145747988/XS48pxJ69LmbBfNk7YSkeUBOhVFFiOWm8xMBQ6-C44XCncKeWRkR02cYoD2y1wZe11eb" -- Reemplaza con la URL de tu webhook de Discord
+
+-- Evento que se dispara cuando el servidor se reinicia
+AddEventHandler('onServerResourceStart', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        local message = "SERVIDOR ONNNNN TODOS DENTRO CONNECT LOCAL HOST LJ SCRIPTS!!!!!!!!" -- Texto 
+        mensaje(message)
+    end
+end)
+
+-- Función para enviar un mensaje a Discord
+function mensaje(content)
+    local requestData = {
+        content = content
+    }
+
+    PerformHttpRequest(discordWebhook, function()
+    end, 'POST', json.encode(requestData), { ['Content-Type'] = 'application/json' })
+end
+
+
